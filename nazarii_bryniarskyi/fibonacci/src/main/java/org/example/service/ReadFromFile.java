7bb471931066d0ac69617d7780a7705e47594a99
Markian Goinets <113860048+Gongameros@users.noreply.github.com>
2024-03-05 08:38:09 +0200

    Revert "Release/fibonacci"

--- nazarii_bryniarskyi/fibonacci/src/main/java/org/example/service/ReadFromFile.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface ReadFromFile<T> {
-
-    List<T> readArray(final String filePath) throws IOException;
-}
