277f980addda5c5493df9f20272331b98b204e6e
OksanaTerletska <telezka.oksana@gmail.com>
2024-03-10 16:52:15 +0200

    TO-01: added task matrix

---
+++ terletska_oksana/Matrix/Matrix/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<List<List<int>>> matrices = new List<List<List<int>>>();
+
+            List<List<int>> matrix1 = readMatrixFromFile("matrix_first.txt");
+            List<List<int>> matrix2 = readMatrixFromFile("matrix_second.txt");
+
+            matrices.Add(matrix1);
+            matrices.Add(matrix2);
+
+            Matrix matrix = new Matrix(matrices);
+
+            List<List<int>> resultAdd = matrix.addMatrix();
+            writeMatrixToFile("Addition: ", resultAdd, "result.txt");
+
+            List<List<int>> resultSubtract = matrix.subMatrices();
+            writeMatrixToFile("Subtraction: ", resultSubtract, "result.txt");
+
+            List<List<int>> resultMultiply = matrix.multiplyMatrices();
+            writeMatrixToFile("Multiplication: ", resultMultiply, "result.txt");
+
+            Console.WriteLine("Matrices are saved in a file.");
+        }
+
+        catch (InvalidOperationException e)
+        {
+            Console.WriteLine("Invalid operation: " + e.Message);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine("File not found: " + e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("The 'try catch' is finished.");
+        }
+    }
+
+    static List<List<int>> readMatrixFromFile(string fileName)
+    {
+        List<List<int>> matrix = new List<List<int>>();
+
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                List<int> row = new List<int>();
+                string[] values = line.Split(',');
+                foreach (string value in values)
+                {
+                    row.Add(int.Parse(value));
+                }
+                matrix.Add(row);
+            }
+        }
+
+        return matrix;
+    }
+    static void writeMatrixToFile(string label, List<List<int>> matrix, string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName, true))
+        {
+            writer.WriteLine(label);
+            foreach (var row in matrix)
+            {
+                foreach (var item in row)
+                {
+                    writer.Write(item + " ");
+                }
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+        }
+    }
+}
---
+++ terletska_oksana/Matrix/Matrix/class Matrix.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+class Matrix
+{
+    private List<List<List<int>>> matrices;
+
+    public Matrix(List<List<List<int>>> matrices)
+    {
+        this.matrices = matrices;
+    }
+
+    public void dimensionalityOfMatrix()
+    {
+        int rowCount = matrices[0].Count;
+        int colCount = matrices[0][0].Count;
+
+        foreach (var matrix in matrices)
+        {
+            if (matrix.Count != rowCount || matrix[0].Count != colCount)
+            {
+                throw new InvalidOperationException("Matrices must have the same dimensions.");
+            }
+        }
+    }
+
+
+    public List<List<int>> addMatrix()
+    {
+        dimensionalityOfMatrix();
+
+        int rowCount = matrices[0].Count;
+        int colCount = matrices[0][0].Count;
+
+        List<List<int>> result = new List<List<int>>();
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            List<int> newRow = new List<int>();
+            for (int j = 0; j < colCount; j++)
+            {
+                int sum = 0;
+                foreach (var matrix in matrices)
+                {
+                    sum += matrix[i][j];
+                }
+                newRow.Add(sum);
+            }
+            result.Add(newRow);
+        }
+        return result;
+    }
+
+    public List<List<int>> subMatrices()
+    {
+        dimensionalityOfMatrix();
+
+        int rowCount = matrices[0].Count;
+        int colCount = matrices[0][0].Count;
+
+        List<List<int>> result = new List<List<int>>();
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            List<int> newRow = new List<int>();
+            for (int j = 0; j < colCount; j++)
+            {
+                int difference = matrices[0][i][j];
+                for (int k = 1; k < matrices.Count; k++)
+                {
+                    difference -= matrices[k][i][j];
+                }
+                newRow.Add(difference);
+            }
+            result.Add(newRow);
+        }
+        return result;
+    }
+
+    public List<List<int>> multiplyMatrices()
+    {
+        if (matrices[0][0].Count != matrices[1].Count)
+        {
+            throw new InvalidOperationException("Number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication.");
+        }
+
+        int rowCountA = matrices[0].Count;
+        int colCountA = matrices[0][0].Count;
+        int rowCountB = matrices[1].Count;
+        int colCountB = matrices[1][0].Count;
+
+        List<List<int>> result = new List<List<int>>();
+
+        for (int i = 0; i < rowCountA; i++)
+        {
+            List<int> newRow = new List<int>();
+            for (int j = 0; j < colCountB; j++)
+            {
+                newRow.Add(0);
+            }
+            result.Add(newRow);
+        }
+
+        for (int i = 0; i < rowCountA; i++)
+        {
+            for (int j = 0; j < colCountB; j++)
+            {
+                for (int k = 0; k < rowCountB; k++)
+                {
+                    result[i][j] += matrices[0][i][k] * matrices[1][k][j];
+                }
+            }
+        }
+        return result;
+    }
+}
