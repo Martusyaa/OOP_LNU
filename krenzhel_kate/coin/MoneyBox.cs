256cd82714ac57721f075929a67a2fd9ca26c38e
katekrenzhel <kate.kr.lv5@gmail.com>
2024-03-17 19:54:53 +0200

    KK-01: add coin

---
+++ krenzhel_kate/coin/MoneyBox.cs
@@ -0,0 +1,32 @@
+﻿public class MoneyBox
+{
+    List<Coin> MyMoney = new List<Coin>();
+    public void AddCoin(Coin coin)
+    {
+        MyMoney.Add(coin);
+    }
+    // public void RemoveCoin(Coin coin)
+    // {
+    //     MyMoney.Remove(coin);
+    // }
+    public int CountCoins()
+    {
+        int totalCoins = 0;
+        foreach (Coin coin in MyMoney)
+        {
+            totalCoins += coin.GetCoins();
+        }
+        return totalCoins;
+    }
+    public void PrintMoney()
+    {
+        Console.WriteLine("Coins in the money box:");
+        foreach (var group in MyMoney.GroupBy(coin => coin.GetSymbol()))
+        {
+            int totalCoins = group.Sum(coin => coin.GetCoins());
+            Console.WriteLine($"{totalCoins} {group.Key}");
+        }
+
+    }
+    
+}
