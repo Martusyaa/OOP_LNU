cb9c63e9aba76fa55af5b6bed4cfbb972badc3ad
maxproschin <polodanka@gmail.com>
2024-03-19 23:02:56 +0200

    added_files

---
+++ proshchyn_maxym/bridge/Color.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    abstract public class Color
+    {
+        public abstract string FillColor();
+
+    }
+
+    public class Red : Color
+    {
+        public override string FillColor()
+        {
+            return "Red";
+        }
+    }
+
+    public class Blue : Color
+    {
+        public override string FillColor()
+        {
+            return "Blue";
+        }
+    }
+
+    public class Green : Color
+    {
+        public override string FillColor()
+        {
+            return "Green";
+        }
+    }
+
+}
