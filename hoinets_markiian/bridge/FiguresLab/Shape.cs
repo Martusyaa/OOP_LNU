563a0fb031b527bb3812db0933b21dbd7522676f
Markiian <markiangonmark@gmail.com>
2024-03-19 12:46:19 +0200

    HM-01: add bridge implementation

---
+++ hoinets_markiian/bridge/FiguresLab/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLab.Figure
+{
+    public abstract class Shape
+    {
+        protected Color _color;
+
+        public Shape(Color color)
+        {
+            this._color = color;
+        }
+
+        public void ShowColor()
+        {
+            Console.WriteLine(_color.ReturnColor());
+        }
+
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+    }
+
+}
