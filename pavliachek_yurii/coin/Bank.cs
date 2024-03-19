28a7d0fdb97b09359058f4e673ef858e7e04fe00
yura <urapavliachek@gmail.com>
2024-03-19 19:44:22 +0200

    PY-01 added files

---
+++ pavliachek_yurii/coin/Bank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace Coin
+{
+    class Bank
+    {
+        private List<Coin> list = new List<Coin>();
+
+        public void AddCoin(Coin coin)
+        {
+            list.Add(coin);
+        }
+
+        public void Count()
+        {
+            Dictionary<Coin.Valuta, int> dict = new Dictionary<Coin.Valuta, int>();
+
+            foreach (Coin coin in list)
+            {
+                if (dict.ContainsKey(coin.valuta))
+                {
+                    dict[coin.valuta] += coin.amount;
+                }
+                else
+                {
+                    dict[coin.valuta] = coin.amount;
+                }
+            }
+        }
+        public void PrintCoins()
+        {
+            Console.WriteLine("Piggy bank:");
+
+            foreach (Coin coin in list)
+            {
+                string currencyName;
+                if (coin.valuta == Coin.Valuta.USD)
+                {
+                    currencyName = "dollars";
+                }
+                else if (coin.valuta == Coin.Valuta.EUR)
+                {
+                    currencyName = "euros";
+                }
+                else
+                {
+                    currencyName = "uahs";
+                }
+                Console.WriteLine($"{coin.amount} {currencyName}");
+            }
+        }
+    }
+}
