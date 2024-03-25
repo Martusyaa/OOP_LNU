b154dcd354272c8375d718cb6f1ae5e41d9aa76d
katekrenzhel <kate.kr.lv5@gmail.com>
2024-03-25 17:14:07 +0200

    KK-01: Add bridge

---
+++ krenzhel_kate/bridge/Figures/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Square : Figure
+{
+
+    public Square(Color color, double side) : base(color, side){}
+
+    public override double CalculatePerimeter()
+    {
+        return 4 * side;
+    }
+
+    public override string Name()
+    {
+        return "Square";
+    }
+
+    public override double CalculateArea()
+    {
+        return side * side;
+    }
+}
