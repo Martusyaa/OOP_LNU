49b2dc1876c923c94ff11d876c78b84daa58395e
VeronikaMahdenko <nikamahdenko@gmail.com>
2024-03-25 17:46:55 +0200

    MV-01:Add simple rest

---
+++ mahdenko_veronika/simple_rest/simple_rest/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Rest
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) //Цей метод викликається при запуску додатка і використовується для налаштування різних сервісів, які будуть доступні в додатку.
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)//Цей метод викликається при запуску додатка і використовується для налаштування конвеєра обробки запитів.
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
