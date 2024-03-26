df24f48d6be5255870c9f172c2943500fe7ab3d2
yura <urapavliachek@gmail.com>
2024-03-26 20:27:19 +0200

    PY-01: added files

---
+++ pavliachek_yurii/patterns/c pattern/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+public class Car
+{
+    public string brand { get; set; }
+    public string model { get; set; }
+    public double speed { get; set; }
+
+    public Car(string brand, string model, double speed)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.speed = speed;
+    }
+    public void Increase(double car_speed)
+    {
+        speed += car_speed;
+    }
+    public void Decrease(double car_speed)
+    {
+        if(speed <= car_speed)
+        {
+            throw new ArgumentException("Speed can't be less than 0");
+        }
+        speed -= car_speed;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Brand: {brand}, Model: {model}, Speed: {speed}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("BMW", "X5", 0);
+        car.Print();
+
+        car.Increase(20);
+        car.Print();
+
+        car.Decrease(10);
+        car.Print();
+    }
+}
---
+++ pavliachek_yurii/patterns/s pattern/Observer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IObserver
+{
+    void Update(string message);
+}
+
+public class ConcreteObserver : IObserver
+{
+    private string name;
+
+    public ConcreteObserver(string name)
+    {
+        this.name = name;
+    }
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"{name} received a message: {message}");
+    }
+}
+
+public interface ISubject
+{
+    void RegisterObserver(IObserver observer);
+    void RemoveObserver(IObserver observer);
+    void NotifyObservers(string message);
+}
+
+public class ConcreteSubject : ISubject
+{
+    private List<IObserver> observers = new List<IObserver>();
+
+    public void RegisterObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObservers(string message)
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update(message);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IObserver observer1 = new ConcreteObserver("Observer 1");
+        IObserver observer2 = new ConcreteObserver("Observer 2");
+        IObserver observer3 = new ConcreteObserver("Observer 3");
+
+        ISubject subject = new ConcreteSubject();
+
+        subject.RegisterObserver(observer1);
+        subject.RegisterObserver(observer2);
+        subject.RegisterObserver(observer3);
+
+        subject.NotifyObservers("The new message!");
+    }
+}
